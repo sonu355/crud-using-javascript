e16de8ca3a41b0bf0318b53f117a5dada158769e
harshal <harshalc562@gmail.com>
2023-12-13 11:18:44 +0530

    list is dyanmically added to the list

---
+++ main.js
@@ -0,0 +1,34 @@
+//selectors
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//event listeners
+todoButton.addEventListener('click', addTodo)
+
+//functions
+function addTodo(e){
+    e.preventDefault();
+    //create li
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo')
+
+    //crate new list
+    const newTodo = document.createElement('li')
+    newTodo.innerText = 'hey'
+    newTodo.classList.add('todo-item')
+    todoDiv.appendChild(newTodo)
+
+    //add the button
+    const checkButton = document.createElement('button')
+    checkButton.innerHTML = '<i class="fas fa-check"></i>';
+    checkButton.classList.add('complete-btn')
+    todoDiv.appendChild(checkButton)
+
+    //add the delete button
+    const deleteButton = document.createElement('button')
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add('delete-btn')
+    todoDiv.appendChild(deleteButton)
+
+    todoList.appendChild(todoDiv)
+}
